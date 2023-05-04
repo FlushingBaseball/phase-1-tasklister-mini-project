2d09cfeb23ed66ba187ac7879419a91e64f5436c
Sam Peck <fullstacksam@protonmail.com>
2023-05-04 12:23:25 -0400

    selector not working, but its a strech goal

--- src/index.js
+++ src/index.js
@@ -1,3 +1,47 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  
+  //create a varaible to store the div form 
+  // that div includes the lable , txtbox and submit button
+  const form = document.querySelector(`#create-task-form`)
+
+
+  const task =document.getElementById('tasks') //adding an EventListener to the submit button that fires on click
+  const option = document.getElementById('select')
+
+  form.addEventListener('submit', (event)=> { //start an event listiner 
+    event.preventDefault() //prevents page refresh
+    const taskTxt = event.target['new-task-description'].value //make a variable taskTxt that is assigned the value of whats entered
+    //into the new-task-description 
+    const list = document.querySelector('#tasks')
+    const newLi = document.createElement('Li')
+    const newbtn = document.createElement('button')
+    // const selectPriority = document.createElement('select')
+    newLi.textContent = taskTxt 
+    newbtn.textContent = 'Delete Task';
+    newbtn.addEventListener('click', handleDelete)
+
+    list.appendChild(newLi).appendChild(newbtn)
+    //console.log(newLi);
+    
+  })  
+  
+  function handleDelete(event){
+    event.target.parentNode.remove()
+  }
+
+
+
+  // task.hasChildNodes.target.addEventListener('click', (event)=>{
+  //  // const element =document.getElementById("newbtn")
+  // remove()
+  
+  // })
+
+
 });
+
+
+
+
+
